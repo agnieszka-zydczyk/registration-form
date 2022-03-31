bc8c8750b6c62d49a4138563a210002ba8c25cb6
AZ <agnieszkayd@gmail.com>
2022-03-31 15:36:51 +0200

    upgraded form & css styles (work in progress)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup>
-
-</script>
-
 <template>
   <header>
 
@@ -9,22 +5,73 @@
 
   <main>
     <div class="wrap">
-      <div id="head">
-        PLEASE REGISTER NOW
-      </div>
       <form>
-        <input class="input is-primary" required placeholder="name" />
-        <input class="input is-primary" placeholder="middle name/s" />
-        <input class="input is-primary" required placeholder="surname" />
-        <input class="input is-primary" />
-        <input class="input is-primary" />
-        <input class="input is-primary" />
+        <h1 id="head">REGISTRATION FORM</h1>
+        <fieldset>
+          <legend>name</legend>
+          <div>
+            <label for="title">personal title</label>
+            <select class="input is-primary" name="title" id="title" required>
+              <option>--- Please choose an option ---</option>
+              <option value="1">Ms</option>
+              <option value="2">Mr</option>
+              <option value="3"></option>
+            </select>
+          </div>
+          <input type="text" class="input is-primary" required placeholder="first name" />
+          <input type="text" class="input is-primary" placeholder="middle name/s (optional)" />
+          <input type="text" class="input is-primary" required placeholder="surname" />
+        </fieldset>
+        <fieldset>
+          <legend>date of birth</legend>
+          <flatPickr class="input is-primary" v-model="date" :config="config" placeholder="select a date" required />
+        </fieldset>
+        <fieldset>
+          <legend>e-mail</legend>
+          <input class="input is-primary" type="email" id="email" placeholder="example@gmail.com" required />
+        </fieldset>
+        <fieldset>
+          <legend>password</legend>
+          <div>
+            <label for="pass">password (8 characters minimum):</label>
+            <input class="input is-primary" type="password" id="pass" name="pass"
+                   minlength="8" required>
+          </div>
+          <div>
+            <label for="p-repeat">confirm password:</label>
+            <input class="input is-primary" type="password" id="p-repeat" name="p-repeat"
+                   minlength="8" required>
+          </div>
+        </fieldset>
+        <div id="buttons">
+          <input class="button" id="reset" type="reset" value="RESET">
+          <input class="button" id="submit" type="submit" value="SIGN UP">
+        </div>
       </form>
-      <button type="submit">SUBMIT</button>
-      <button type="reset">RESET</button>
     </div>
   </main>
 </template>
+
+<script>
+
+// import { matchPassword } from './main.js';
+import flatPickr from 'vue-flatpickr-component';
+import 'flatpickr/dist/flatpickr.css';
+
+export default {
+  data() {
+    return {
+      date: null,
+      config: {
+        maxDate: 'today',
+      },
+    };
+  },
+  components: {
+    flatPickr
+  }
+};
+</script>
 
 <style>
 @import "./assets/base.css";
@@ -33,21 +80,38 @@ main {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 150%;
+  height: 100%;
 }
 
 .is-primary {
-  color: hsl(217, 71%, 53%);
+  color: hsl(185, 29%, 32%);
   margin-bottom: 0.75rem;
   max-width: 40rem;
 }
-
+.wrap{
+  background-color: rgba(29, 215, 229, 0.3);
+  padding: 20px;
+}
 #head {
   margin-bottom: 2rem;
-  height: 150%;
+  height: 100%;
+  background-color: rgb(30, 221, 221);
+  padding: 40px;
+}
+form{
+
+}
+#buttons {
+
 }
 
-form {
+#submit {
+  margin-left: 570px;
 }
+
+#reset {
+
+}
+
 </style>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount("#app");
+createApp( App ).mount( '#app' );
+
+// export function matchPassword() {
+//     const pw1 = document.getElementById( 'pass' );
+//     const pw2 = document.getElementById( 'p-repeat' );
+//     if ( pw1 != pw2 ) {
+//         alert( 'Passwords did not match' );
+//     };
+// }
